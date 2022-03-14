a3ad49186b61fa8811b7ed9a0715cd40962ecd43
Jisang Hong <wona23@gmail.com>
2022-03-14 16:42:07 +0900

    Tailwind CSS 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-blue-600">
+      <h1 className="text-yellow-200">it works</h1>
+    </div>
+  );
 };
 
 export default Home;
