d09df5d6f9ef0dfd0747a4fa34ae80729f62650f
Jisang Hong <wona23@gmail.com>
2022-04-06 15:28:28 +0900

    User 찾기 및 생성 로직 작성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,58 @@
+import client from '@libs/server/client';
 import withHandler from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  res.status(200).end();
+  const { email, phone } = req.body;
+  const payload = email ? { email } : { phone: +phone };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: 'Anonymous',
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+  /* if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log('사용자를 찾았습니다.');
+    if (!user) {
+      console.log('사용자를 찾지 못했습니다. 사용자를 새로 등록합니다.');
+      user = await client.user.create({
+        data: {
+          name: 'Anonymous',
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log('사용자를 찾았습니다.');
+    if (!user) {
+      console.log('사용자를 찾지 못했습니다. 사용자를 새로 등록합니다.');
+      user = await client.user.create({
+        data: {
+          name: 'Anonymous',
+          phone: +phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
+  return res.status(200).end();
 }
 
 export default withHandler('POST', handler);
