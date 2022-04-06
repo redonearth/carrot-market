2efd52312d7e2aff6c3d2801204f7ab0e074897f
Jisang Hong <wona23@gmail.com>
2022-04-06 16:54:23 +0900

    사용자 인증 로직 개선

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,10 +1,14 @@
 import client from '@libs/server/client';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { IResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseType>
+) {
   const { email, phone } = req.body;
-  const user = email ? { email } : { phone: +phone };
+  const user = email ? { email } : phone ? { phone: +phone } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + '';
   const token = await client.token.create({
     data: {
@@ -22,44 +26,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     },
   });
-  console.log(token);
-  /* if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log('사용자를 찾았습니다.');
-    if (!user) {
-      console.log('사용자를 찾지 못했습니다. 사용자를 새로 등록합니다.');
-      user = await client.user.create({
-        data: {
-          name: 'Anonymous',
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log('사용자를 찾았습니다.');
-    if (!user) {
-      console.log('사용자를 찾지 못했습니다. 사용자를 새로 등록합니다.');
-      user = await client.user.create({
-        data: {
-          name: 'Anonymous',
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  } */
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler('POST', handler);
