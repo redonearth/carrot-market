7b97606dd1a162872491e25546debd02a64c3955
Jisang Hong <wona23@gmail.com>
2022-04-07 16:44:07 +0900

    Serverless Session 로직 구현

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,35 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
 import client from '@libs/server/client';
 import withHandler, { IResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: {
+      id: number;
+    };
+  }
+}
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) return res.status(404).end();
+  req.session.user = {
+    id: exists.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler('POST', handler);
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrot-session',
+  password: 'ag7z9x987wrwq79812nv971h8abv83zb',
+});
