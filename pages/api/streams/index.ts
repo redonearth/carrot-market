4acee0ab35769c721717b80d6814a3f6b99380d8
Jisang Hong <wona23@gmail.com>
2022-06-10 14:28:39 +0900

    무한 스크롤 기능 구현

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -10,12 +10,18 @@ async function handler(
   const {
     session: { user },
     body: { name, price, description },
+    query: { page },
   } = req;
   if (req.method === 'GET') {
-    const streams = await client.stream.findMany();
+    const streamCount = await client.stream.count();
+    const streams = await client.stream.findMany({
+      take: 10,
+      skip: (+page - 1) * 10,
+    });
     res.json({
       ok: true,
       streams,
+      pages: Math.ceil(streamCount / 10),
     });
   }
   if (req.method === 'POST') {
--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -3,19 +3,33 @@ import Link from 'next/link';
 import FloatingButton from '@components/floating-button';
 import Layout from '@components/layout';
 import { Stream } from '@prisma/client';
-import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
+import { useInfiniteScroll } from '@libs/client/useInfiniteScroll';
+import { useEffect } from 'react';
 
 interface IStreamsResponse {
   ok: boolean;
   streams: Stream[];
+  pages: number;
 }
 
+const getKey = (pageIndex: number, previousPageData: IStreamsResponse) => {
+  if (pageIndex === 0) return `/api/streams?page=1`;
+  if (pageIndex + 1 > previousPageData.pages) return null;
+  return `/api/streams?page=${pageIndex + 1}`;
+};
+
 const Streams: NextPage = () => {
-  const { data } = useSWR<IStreamsResponse>(`/api/streams`);
+  const { data, setSize } = useSWRInfinite<IStreamsResponse>(getKey);
+  const streams = data ? data.map((item) => item.streams).flat() : [];
+  const page = useInfiniteScroll();
+  useEffect(() => {
+    setSize(page);
+  }, [setSize, page]);
   return (
     <Layout title="라이브" hasTabBar>
       <div className="space-y-4 divide-y-[1px]">
-        {data?.streams.map((stream) => (
+        {streams.map((stream) => (
           <Link key={stream.id} href={`/streams/${stream.id}`}>
             <a className="block px-4 pt-4">
               <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
