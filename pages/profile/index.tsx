950937a864f8a34f3ad6a849a6bffa9e11fec2b7
Jisang Hong <wona23@gmail.com>
2022-06-28 14:09:20 +0900

    SSR + User Authentication 적용

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,13 +1,14 @@
-import type { NextPage } from 'next';
+import type { NextPage, NextPageContext } from 'next';
 import Link from 'next/link';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
-import useSWR from 'swr';
+import useSWR, { SWRConfig } from 'swr';
 import { Review, User } from '@prisma/client';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { joinClassNames } from '@libs/client/utils';
 import Image from 'next/image';
+import { withSsrSession } from '@libs/server/withSession';
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -159,4 +160,36 @@ const Profile: NextPage = () => {
   );
 };
 
-export default Profile;
+const Page: NextPage<{ profile: User }> = ({ profile }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          '/api/users/me': {
+            ok: true,
+            profile,
+          },
+        },
+      }}
+    >
+      <Profile />
+    </SWRConfig>
+  );
+};
+
+export const getServerSideProps = withSsrSession(async function ({
+  req,
+}: NextPageContext) {
+  const profile = await client?.user.findUnique({
+    where: {
+      id: req?.session.user?.id,
+    },
+  });
+  return {
+    props: {
+      profile: JSON.parse(JSON.stringify(profile)),
+    },
+  };
+});
+
+export default Page;
