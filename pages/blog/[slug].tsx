edccad47556bd5f6a7343c8692872a66cc296297
Jisang Hong <wona23@gmail.com>
2022-06-28 15:38:12 +0900

    getStaticPaths 추가

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,40 @@
+import { readdirSync } from 'fs';
+import matter from 'gray-matter';
+import type { GetStaticProps, NextPage } from 'next';
+import remarkHtml from 'remark-html';
+import remarkParse from 'remark-parse';
+import { unified } from 'unified';
+
+const Post: NextPage<{ post: string }> = ({ post }) => {
+  return <h1>{post}</h1>;
+};
+
+export function getStaticPaths() {
+  const files = readdirSync('./posts').map((file) => {
+    const [name, extension] = file.split('.');
+    return {
+      params: {
+        slug: name,
+      },
+    };
+  });
+  return {
+    paths: files,
+    fallback: false,
+  };
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { content } = matter.read(`./posts/${ctx.params?.slug}.md`);
+  const { value } = await unified()
+    .use(remarkParse)
+    .use(remarkHtml)
+    .process(content);
+  return {
+    props: {
+      post: value,
+    },
+  };
+};
+
+export default Post;
