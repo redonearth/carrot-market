49b617abab90961100aea8139321bfe494298dcc
Jisang Hong <wona23@gmail.com>
2022-06-30 17:02:34 +0900

    컴포넌트 분리, Suspense 적용

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage, NextPageContext } from 'next';
+import type { NextPage } from 'next';
 import Link from 'next/link';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
@@ -10,6 +10,7 @@ import { joinClassNames } from '@libs/client/utils';
 import Image from 'next/image';
 import { withSsrSession } from '@libs/server/withSession';
 import client from '@libs/server/client';
+import { Suspense } from 'react';
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -20,35 +21,87 @@ interface IReviewsResponse {
   reviews: ReviewWithUser[];
 }
 
-const Profile: NextPage = () => {
+const MiniProfile = () => {
   const { user } = useUser();
-  const { data } = useSWR<IReviewsResponse>('/api/reviews');
+  return (
+    <>
+      <div className="mt-4 flex items-center space-x-3">
+        {user?.avatar ? (
+          <div className="relative h-16 w-16 rounded-full">
+            <Image
+              src={`https://imagedelivery.net/Y45BUDi393Qe7-mR-gFRlA/${user?.avatar}/avatar`}
+              layout="fill"
+              className="rounded-full object-cover"
+              alt="Avatar"
+              priority={true}
+            />
+          </div>
+        ) : (
+          <div className="h-16 w-16 rounded-full bg-slate-500" />
+        )}
+        <div className="flex flex-col">
+          <span className="font-medium text-gray-900">{user?.name}</span>
+          <Link href="/profile/edit">
+            <a className="text-sm text-gray-700">프로필 편집 &rarr;</a>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Reviews = () => {
+  const { data } = useSWR<IReviewsResponse>(
+    // '/api/reviews'
+    typeof window === 'undefined' ? null : '/api/reviews'
+  );
+  return (
+    <>
+      {data?.reviews.map((review) => (
+        <div key={review.id} className="mt-12">
+          <div className="flex items-center space-x-4">
+            <div className="h-12 w-12 rounded-full bg-slate-400" />
+            <div>
+              <h4 className="text-sm font-bold text-gray-800">
+                {review.createdBy.name}
+              </h4>
+              <div className="flex items-center">
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <svg
+                    key={star}
+                    className={joinClassNames(
+                      'h-5 w-5',
+                      review.score >= star ? 'text-yellow-400' : 'text-gray-400'
+                    )}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                  </svg>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 text-sm text-gray-600">
+            <div>
+              <p>{review.review}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+const Profile: NextPage = () => {
   return (
     <Layout title="나의 캐럿" seoTitle="나의 캐럿" hasTabBar>
       <div className="px-4">
-        <div className="mt-4 flex items-center space-x-3">
-          {user?.avatar ? (
-            <div className="relative h-16 w-16 rounded-full">
-              <Image
-                src={`https://imagedelivery.net/Y45BUDi393Qe7-mR-gFRlA/${user?.avatar}/avatar`}
-                layout="fill"
-                className="rounded-full object-cover"
-                alt="Avatar"
-                priority={true}
-              />
-            </div>
-          ) : (
-            <div className="h-16 w-16 rounded-full bg-slate-500" />
-          )}
-          <div className="flex flex-col">
-            <span className="font-medium text-gray-900">
-              {user ? user?.name : <Skeleton />}
-            </span>
-            <Link href="/profile/edit">
-              <a className="text-sm text-gray-700">프로필 편집 &rarr;</a>
-            </Link>
-          </div>
-        </div>
+        <Suspense fallback={'Loading Mini Profile'}>
+          <MiniProfile />
+        </Suspense>
         <div className="mt-10 flex justify-around">
           <Link href="/profile/sold">
             <a className="flex flex-col items-center">
@@ -120,57 +173,19 @@ const Profile: NextPage = () => {
             </a>
           </Link>
         </div>
-        {data?.reviews.map((review) => (
-          <div key={review.id} className="mt-12">
-            <div className="flex items-center space-x-4">
-              <div className="h-12 w-12 rounded-full bg-slate-400" />
-              <div>
-                <h4 className="text-sm font-bold text-gray-800">
-                  {review.createdBy.name}
-                </h4>
-                <div className="flex items-center">
-                  {[1, 2, 3, 4, 5].map((star) => (
-                    <svg
-                      key={star}
-                      className={joinClassNames(
-                        'h-5 w-5',
-                        review.score >= star
-                          ? 'text-yellow-400'
-                          : 'text-gray-400'
-                      )}
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <div className="mt-4 text-sm text-gray-600">
-              <div>
-                <p>{review.review}</p>
-              </div>
-            </div>
-          </div>
-        ))}
+        <Suspense fallback={'Loading Reviews'}>
+          <Reviews />
+        </Suspense>
       </div>
     </Layout>
   );
 };
 
-const Page: NextPage<{ profile: User }> = ({ profile }) => {
+const Page: NextPage = () => {
   return (
     <SWRConfig
       value={{
-        fallback: {
-          '/api/users/me': {
-            ok: true,
-            profile,
-          },
-        },
+        suspense: true,
       }}
     >
       <Profile />
@@ -178,7 +193,7 @@ const Page: NextPage<{ profile: User }> = ({ profile }) => {
   );
 };
 
-export const getServerSideProps = withSsrSession(async function ({
+/* export const getServerSideProps = withSsrSession(async function ({
   req,
 }: NextPageContext) {
   const profile = await client.user.findUnique({
@@ -191,6 +206,6 @@ export const getServerSideProps = withSsrSession(async function ({
       profile: JSON.parse(JSON.stringify(profile)),
     },
   };
-});
+}); */
 
 export default Page;
