87a74f7125439a224de3589ac6f7ae8ac390a586
Jisang Hong <wona23@gmail.com>
2024-04-15 14:41:23 +0900

    feat(login): 이메일 로그인 기능 구현

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,20 +1,40 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
 import { z } from "zod";
+import { compare } from "bcrypt";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
-  password: z
-    .string({
-      required_error: "비밀번호를 입력하세요.",
-    })
-    .min(PASSWORD_MIN_LENGTH)
-    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "이메일이 없습니다."),
+  password: z.string({
+    required_error: "비밀번호를 입력하세요.",
+  }),
+  // .min(PASSWORD_MIN_LENGTH)
+  // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
 export async function login(prevState: any, formData: FormData) {
@@ -23,11 +43,35 @@ export async function login(prevState: any, formData: FormData) {
     password: formData.get("password"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    const ok = await compare(result.data.password, user!.password ?? "");
+
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["잘못된 비밀번호입니다."],
+          email: [],
+        },
+      };
+    }
   }
 }
