9b64fd1c6f784acfa319c67126996d3591cde23e
Jisang Hong <wona23@gmail.com>
2024-04-15 15:09:38 +0900

    feat: profile 페이지 로그아웃 기능 구현

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,41 @@
-export default function Profile() {
-  return <div>Profile</div>;
+import { notFound, redirect } from "next/navigation";
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+
+async function getUser() {
+  const session = await getSession();
+
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+  const logOut = async () => {
+    "use server";
+
+    const session = await getSession();
+    session.destroy();
+
+    redirect("/");
+  };
+
+  return (
+    <div>
+      <h1>환영합니다 🙌 {user?.username}</h1>
+      <form action={logOut}>
+        <button className="font-bold text-blue-500">로그아웃</button>
+      </form>
+    </div>
+  );
 }
