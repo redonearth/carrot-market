19ef69b25fb906eb052a17d16d1252c64f96ef26
Jisang Hong <wona23@gmail.com>
2024-04-17 08:39:55 +0900

    feat: Products 로딩 skeleton 컴포넌트 구현

---
+++ app/(tabs)/products/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div className="p-5 animate-pulse flex flex-col gap-y-5">
+      {[...Array(10)].map((_, index) => (
+        <div className="*:rounded-md flex gap-x-5">
+          <div className="size-28 rounded-md bg-neutral-700" />
+          <div className="flex flex-col gap-y-2 *:rounded-md">
+            <div className="bg-neutral-700 h-5 w-40" />
+            <div className="bg-neutral-700 h-5 w-20" />
+            <div className="bg-neutral-700 h-5 w-10" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
