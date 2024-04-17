8b4f118f126477e5626080e0730a62bb2d840231
Jisang Hong <wona23@gmail.com>
2024-04-17 12:45:25 +0900

    feat: 상품 목록 무한 스크롤링 구현

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
 import ListProduct from "./list-product";
@@ -15,36 +15,57 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [page, setPage] = useState(0);
   const [isLastPage, setIsLastPage] = useState(false);
 
-  const handleClickLoadMore = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page + 1);
-    if (newProducts.length !== 0) {
-      setPage((prev) => prev + 1);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
+  const trigger = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (entries, observer) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+
+          const newProducts = await getMoreProducts(page + 1);
+
+          if (newProducts.length !== 0) {
+            setPage((prev) => prev + 1);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+
+          setIsLoading(false);
+        }
+      },
+      {
+        threshold: 1.0,
+        rootMargin: "0px 0px -100px 0px",
+      }
+    );
+
+    if (trigger.current) {
+      observer.observe(trigger.current);
     }
-    setIsLoading(false);
-  };
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [page]);
 
   return (
     <div className="p-5 flex flex-col gap-y-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {isLastPage ? (
-        <span className="mx-auto font-semibold text-sm text-gray-400 py-2">
-          더 보여줄 게 없어요 😅
-        </span>
-      ) : (
-        <button
-          onClick={handleClickLoadMore}
-          disabled={isLoading}
+
+      {!isLastPage ? (
+        <span
+          ref={trigger}
           className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? "로딩 중" : "더 보기"}
-        </button>
-      )}
+        </span>
+      ) : null}
     </div>
   );
 }
