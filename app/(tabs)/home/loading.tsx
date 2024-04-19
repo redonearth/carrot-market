3c209cb2e374382f34e1995a64638ad1424b46ad
Jisang Hong <wona23@gmail.com>
2024-04-19 10:28:23 +0900

    style(Home): 로딩 스켈레톤 수정

--- app/(tabs)/home/loading.tsx
+++ app/(tabs)/home/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="p-5 animate-pulse flex flex-col gap-y-5">
       {[...Array(10)].map((_, index) => (
-        <div key={index} className="*:rounded-md flex gap-x-5">
+        <div key={index} className="*:rounded-md flex items-center gap-x-5">
           <div className="size-28 rounded-md bg-neutral-700" />
           <div className="flex flex-col gap-y-2 *:rounded-md">
             <div className="bg-neutral-700 h-5 w-40" />
